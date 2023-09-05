37c687dd55721a9c52d3f752b0b2a5c5d2ee49ef
unknown <lvovslava1@gmail.com>
2023-09-05 21:52:38 +0300

    ttts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', default='1213')
 
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ' ').split(",")
 
 
 # Application definition
